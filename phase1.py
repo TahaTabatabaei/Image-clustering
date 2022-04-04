6d7ae9433abd25e31cef09f8251d103f36f74010
seyed mohammad taha tabatabaei <78843420+TahaTabatabaei@users.noreply.github.com>
2022-04-04 21:21:51 +0430

    heeeey

--- phase1.py
+++ phase1.py
@@ -1,9 +1,11 @@
+from cProfile import label
+from ctypes import sizeof
 from random import random
 from typing import List
 from matplotlib import image
 from matplotlib import pyplot
-from numpy import asarray
 import numpy as np
+from pprint import pprint
 
 
 # print(myImage.dtype)
@@ -19,7 +21,7 @@ k=0
 
 st=""
 imageArr = list()
-for i in range(1,411):
+for i in range(1,401):
     if (i%10 == 1):
         k +=1
     si = str(i)
@@ -27,11 +29,27 @@ for i in range(1,411):
     st = si+"_"+sk
     # print(st)
     myImage = image.imread("E:/taha/code/HW1/src/ORL/"+st+".jpg")
-    print(myImage.shape)
-    data = asarray(myImage)
-    dataFlat = np.ndarray.flatten(data)
+    # print(i ,myImage.shape)
+    # print(myImage)
+    # data = np.asarray(myImage)
+    # print(data)
+    dataFlat = np.ndarray.flatten(myImage)
     imageArr.append(dataFlat)
 
 from sklearn.cluster import KMeans
-kmean = KMeans(init=random,n_clusters=41).fit(imageArr)
-    
+
+# kmeans = KMeans(init="random",n_clusters=40).fit_predict(imageArr)
+# pprint(kmeans)
+
+from sklearn.cluster import DBSCAN
+
+# dbscan = DBSCAN(min_samples=2).fit_predict(imageArr)
+# pprint(dbscan)
+
+from sklearn.cluster import AgglomerativeClustering
+
+# aglo_avg = AgglomerativeClustering(linkage='average',n_clusters=40).fit_predict(imageArr)
+# aglo_single = AgglomerativeClustering(linkage='single',n_clusters=40).fit_predict(imageArr)
+# aglo_comp = AgglomerativeClustering(linkage='complete',n_clusters=40).fit_predict(imageArr)
+
+# pprint(aglo_avg)
