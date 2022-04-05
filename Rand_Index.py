3220ce4767c10dea9cff0cae6ee80fdd18ca6ee5
seyed mohammad taha tabatabaei <78843420+TahaTabatabaei@users.noreply.github.com>
2022-04-05 14:59:52 +0430

    rand index function

---
+++ Rand_Index.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+import numpy as np
+from pprint import pprint
+def rand_Index(labels_true,labels_predicted):
+    trueP=0
+    trueN=0
+    falseN=0
+    falseP=0
+    # print("labels:")
+    # print(labels_true[2])
+    # pprint(labels_true)
+    for i in range(len(labels_true)):
+        for j in range(i+1 ,len(labels_true)):
+            print("i:",i,"j:",j)
+            pair_true = list() 
+            pair_true[0] = labels_true[i]
+            pair_true[1] = labels_true[j]
+            pair_pred = list()
+            pair_pred[0] = labels_predicted[i]
+            pair_pred[1] = labels_predicted[j]
+            if(pair_true[0] == pair_true[1]):
+                if(pair_pred[0] == pair_pred[1]):
+                    trueP +=1
+                else:
+                    falseN +=1
+            else:
+                if(pair_pred[0] == pair_pred[1]):
+                    falseP +=1
+                else:
+                    trueN +=1
+    return (trueP + trueN)
+
